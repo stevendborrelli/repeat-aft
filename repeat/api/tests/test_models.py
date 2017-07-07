92c5da2eb3bbdbc4242c3632e7e263ba37fb83ec
Langston Barrett <langston.barrett@gmail.com>
2017-07-07 00:29:21 +0000

    lazily extract text from papers

--- repeat/api/tests/test_models.py
+++ repeat/api/tests/test_models.py
@@ -1,6 +1,10 @@
+from django.core.files import uploadedfile
 import django.core.files
 import django.core.files.uploadedfile
 import django.test
+import factory
+import faker
+import io
 import json
 
 from . import factories
@@ -24,3 +28,33 @@ class PaperTests(django.test.TestCase):
         self.assertEqual(
             set(['unique_id', 'title', 'authors', 'domains']),
             set(json.loads(str(paper)).keys()))
+
+    def test_update_file(self):
+        paper = factories.Paper.create()
+        content = faker.Faker().text().encode()
+
+        # The paper initially has no document_text
+        with self.assertRaises(ValueError):
+            paper.document_text.file
+
+        paper.document_text = uploadedfile.SimpleUploadedFile("test.dat",
+                                                              content)
+        paper.save()
+
+        self.assertEqual(content, paper.document_text.read())
+
+    def test_get_text(self):
+        # Initially has no document_text attribute
+        blank_paper = factories.Paper.create()
+        self.assertEqual(
+            (test_pdfutil.BLANK_RESULT, False), blank_paper.get_text())
+        self.assertEqual(
+            (test_pdfutil.BLANK_RESULT, True), blank_paper.get_text())
+
+        lorem_paper = factories.Paper.create(document=factory.django.FileField(
+            data=test_pdfutil.LOREM))
+
+        self.assertEqual(
+            (test_pdfutil.LOREM_RESULT, False), lorem_paper.get_text())
+        self.assertEqual(
+            (test_pdfutil.LOREM_RESULT, True), lorem_paper.get_text())
