899880a55baaa237a1aaa3dabb2d0a0aacd10487
Langston Barrett <langston.barrett@gmail.com>
2017-08-03 19:03:16 +0000

    usage docs: OSX requires downloading of NLTK data
    fixes #51

--- repeat/reset.sh
+++ repeat/reset.sh
@@ -2,9 +2,11 @@
 
 # Destroy the current database. For development purposes only!
 
-rm -rf api/migrations
-rm -f db.sqlite3
-rm -f /tmp/repeat.sqlite3
+maybe_delete () { ([[ -f "$1" ]] || [[ -d "$1" ]]) && rm -rf "$1"; }
+
+maybe_delete api/migrations
+maybe_delete db.sqlite3
+maybe_delete /tmp/repeat.sqlite3
 ./manage.py makemigrations
 ./manage.py migrate --run-syncdb
 ./manage.py loaddata ./api/fixtures/fixture.json
